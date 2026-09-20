521a1cb4918768a0742505bde789f71341fb0fa5
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-09-20 11:57:27 +0530

    init

---
+++ backend/2. Install node and File System operations/script.js
@@ -0,0 +1,30 @@
+//Node js is not a 
+//                  -programming langugae X
+//                  -framework X
+//                  -technology X
+//                  -library X
+//                  -software X
+
+
+//Node js in JAVASCRIPT RUN TIME ENVIRONMENT
+
+//-------------------------------------------------
+
+//JS se backend nahi bn skta kyunki js ke pass voh functionalities hi nahi hai jisse backend bnta hai 
+
+//ryan dahl , isne socha js se backend bnna chahiye ###
+
+//mehnt krne ke baad, google chrome V8 engine bnata hai , opensource kr deta hai and ryan dahl do all the operations 
+
+// fir usne socha ki hmein toh js mein code krna hai  , kyunki chrome v8 engine bna hai c++ mein .
+
+//hm js ka code likhenge jo ki wrapper layer of js receinve kregea and voh code v8 engine ke c++ modules ke sath ek server create krega
+
+//yeh poora layer and uske andr v8 is called NODE JS
+
+//===============nodejs is a js runtime environment===========================
+
+//NPM - may be package store .
+
+
+// 1. npm init
