4652a16b992157f09ba5fbeeafe5c9c94d80bf86
Garvit Gandhi <garvitgandhi0313@gmail.com>
2026-09-20 16:21:06 +0530

    add

---
+++ backend/3. npm basics and advanced features/script.js
@@ -0,0 +1,23 @@
+//npm - node package manager
+// but isrf node nahi milta, toh koi defination nahi bnai gyi 
+
+//nodejs core mein installed hoke aata hai , use MODULE kehte hai 
+// ninhe npm se download krte hai , use PACKAGE bolte hai 
+
+
+//Dependencies - package and unki dependencies
+
+//Dev_dependencies - aise packages jo sirf development mein kaam aayenge, pr jb app bn jayega and upload ho jayega tb hm in packages ko use nahi kr rahe honge
+
+
+//===========================================================================================
+//===========================================================================================
+//kai baar aap koi script chlate ho , and us script mein aap likhte ho 
+// npm start 
+// npm run dev
+// npm run concurrent
+//===========================================================================================
+//===========================================================================================
+
+
+// jb hm start ya test likhte hai, toh pehle toh hmare os kjo commands ke bare mein nahi pta hota, toh hm start ya test likhte hai, jisse yeh cheez hmare os ke table mein likh dete hai , and jb bhi start likhne kebad voh likh jata hai table hai, fir hm npm run se apni commands chla skte hai , jo hmne create kri hai 
